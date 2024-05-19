6e28f42f756ed3e368a126ff5fcdf80502ac1db3
Kirill <dorc99@mail.ru>
2024-05-19 16:45:09 +0700

    fix

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  // basePath: "/Beauty-experts",
+  basePath: "/Beauty-experts",
   output: "export",  // <=== enables static exports
   reactStrictMode: true,
 };
